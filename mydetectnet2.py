3c2f863019cd4cff4417e36478222d557adcae94
worker_yza <2934002490@qq.com>
2023-07-23 02:36:17 +0800

    UI界面编写完成进行合并调试

--- mydetectnet2.py
+++ mydetectnet2.py
@@ -25,7 +25,6 @@ import jetson.inference
 import serial as ser
 import time
 import os
-import cv2
 import argparse
 import sys
 from PyQt5.QtWidgets import *
@@ -34,13 +33,12 @@ from PyQt5.QtGui import *
 from mytrash_ui import Ui_MainWindow
 
 
-Recyleable_waste = [7,8]
+Recyleable_waste = [1,2]
 Other_waste = [3]
-Harmful_waste = [1,2]
-Kitchen_waste = [4,5,6]
-global n,flag#检测帧照片命名
-n=1042
-flag=0
+Harmful_waste = [4,5]
+Kitchen_waste = [6,7]
+global n #检测帧照片命名
+n=10
 
 #serial port initialization
 se = ser.Serial("/dev/ttyUSB0",115200,timeout=0.01)
@@ -85,43 +83,40 @@ input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
 #识别函数
 
 def Detect_mine():
-	global n
-	time.sleep(0.2)
+	time.sleep(0.4)
 	img = input.Capture()
-	display.Render(img)
+
 
 	# detect objects in the image (with overlay)
 	detections = net.Detect(img, overlay=opt.overlay)
-	#display.Render(img)
-	#display.SetStatus(str(n)+"-Object Detection |  Network {:.0f}FPS".format(net.GetNetworkFPS()))
+	output = jetson.utils.videoOutput("notdetection")
+	display.Render(img)
+	display.SetStatus(str(n)+"-Object Detection |  Network {:.0f}FPS".format(net.GetNetworkFPS()))
 	jetson.utils.saveImageRGBA(str(n)+'.jpg',img)
-	#cv2.imshow('.jpg',img)
-	#output = jetson.utils.videoOutput("notdetection")
 	#output.Render(img)
+	#os.rename(img,(str)name+'.jpg')
+
+
 
 	# print the detections
 	print("detected {:d} objects in image".format(len(detections)))
 	if format(len(detections)) == '0' :
 		se.write('O'.encode("GB2312"))
-		mwin.plainTextEdit_2.appendPlainText("Other_waste：")
 
 	for detection in detections:
 		if detection.ClassID in Recyleable_waste :
 			se.write('R'.encode("GB2312"))
 			mwin.plainTextEdit_2.appendPlainText('Recyleable_waste：')
-			print("ok")
 		elif detection.ClassID in Other_waste:
 			se.write('O'.encode("GB2312"))
 			mwin.plainTextEdit_2.appendPlainText("Other_waste：")
-			print("ok")
 		elif detection.ClassID in Harmful_waste:
 			se.write('H'.encode("GB2312"))
 			mwin.plainTextEdit_2.appendPlainText('Harmful_waste：')
-			print("ok")
 		elif detection.ClassID in Kitchen_waste:
 			se.write('K'.encode("GB2312"))
 			mwin.plainTextEdit_2.appendPlainText('Kitchen_waste：')
-			print("ok")
+
 		#print(detection)
 		print(detection.ClassID)
 		break
@@ -129,68 +124,47 @@ def Detect_mine():
 
 	#net.PrintProfilerTimes()
 
-#-------------------------------------------------------------------------------------------------------
-#多线程
-class single_detectionThread(QThread):
-	def __init__(self):
-		super().__init__()
-
-	def run(self):
-		global flag
-		while True:
-			receiving_code = se.readline().decode("GB2312")
-			print(receiving_code)
-			if receiving_code == 'A':
-				print("detecting")
-				Detect_mine()
-				print("over")
-			if flag == 1 :
-				break
-
-
 #---------------------------------------------------------------------------------------------------------
 #UI界面
 class MainWindow(Ui_MainWindow, QMainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__()
 		self.setupUi(self)
-		self.mythread = single_detectionThread()
 
 		self.pushButton_5.clicked.connect(self.start_pushbutton)
-		
-		self.pushButton_3.clicked.connect(self.reset_pushbutton)
-		self.pushButton_4.clicked.connect(self.video_start)
+		self.pushButton_2.clicked.connect(self.stop_pushbutton)
 	def start_pushbutton(self):
 		msgbox = QMessageBox.information(self,"系统提示：","开启成功！")
 		self.pushButton_5.setEnabled(True)
 		print('开始识别：\n')
 		self.plainTextEdit_2.appendPlainText('开始识别：')
-		self.mythread.start()
-
-		self.pushButton_2.clicked.connect(self.stop_pushbutton)
-
+		while True:
+			Receiving_code = se.readline().decode("GB2312")
+			print(Receiving_code)
+			if Receiving_code == 'A':
+				print("detecting")
+				Detect_mine()
+				print("over")
 
 	def stop_pushbutton(self):
-		global flag
 		reply = QMessageBox.question(self, '系统提示', '你确定要结束识别？', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 		# event.accept()
 		if reply != QMessageBox.Yes:
 			event.ignore()
 		else:
-			flag = 1
-			self.mythread.quit()
 			print('结束所有任务\n')
 			self.plainTextEdit_2.appendPlainText('结束所有任务！')
 		self.pushButton_2.setEnabled(False)
 
+		self.pushButton_3.clicked.connect(self.reset_pushbutton)
 
 	def reset_pushbutton(self):
 		self.pushButton_5.setEnabled(True)
 		self.pushButton_2.setEnabled(True)
 		self.plainTextEdit_2.appendPlainText('重新启动：')
 		self.plainTextEdit_2.setPlainText("")  # 清屏
-		self.mythread.start()
 
+		self.pushButton_4.clicked.connect(self.video_start)
 
 	def video_start(self):
 		self.plainTextEdit_2.appendPlainText('视频播放中...')
@@ -198,11 +172,21 @@ class MainWindow(Ui_MainWindow, QMainWindow):
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
+
 	mwin = MainWindow()
 	mwin.show()
+
 	sys.exit(app.exec_())
 
 # process frames until the user exitsRecyleable
+#while True:
+
+	#Receiving_code = se.readline().decode("GB2312")
+	#print(Receiving_code)
+	#if Receiving_code == 'A' :
+	#	print("detecting")
+	#	Detect_mine()
+	#	print("over")
 	# capture the next image
 
 
